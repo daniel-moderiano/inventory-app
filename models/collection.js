6535a638c6613c2652afe6e2149ce643a1edbf11
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-15 10:38:22 +1030

    Create collectino schema

---
+++ models/collection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the schema for an NFT collection (a.k.a. brand of NFT, e.g. Cryptopunks, Bored Ape Yacht Club, etc.)
+const CollectionSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    numberOfItems: { type: Number }  // Calculated using NFTs with this collection ID
+  } 
+);
+
+// Virtual for collection URL. Note an arrow function is not used in the getter to avoid incorrect 'this' binding
+CollectionSchema
+.virtual('url')
+.get(function () {
+  return '/' + this._id;
+});
+
+// Export the Schema as a mongoose model. A model instance can be considered an actual document to be saved/updated/deleted from a MongoDB collection
+module.exports = mongoose.model('Collection', CollectionSchema);
