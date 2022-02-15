c1d0023cf6800400411bbaa81db3dfaf56a935fc
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-15 10:34:27 +1030

    Create creator schema

---
+++ models/creator.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the schema for an NFT creator, using the Schema class in Mongoose. This essentially defines the fields and values permitted for a creator document in MongoDB
+const CreatorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    numberCreated: { type: Number } // will be calculated by finding all NFTs with this creator ID
+  } 
+);
+
+// Virtual for creator's URL. Note an arrow function is not used in the getter to avoid incorrect 'this' binding
+CreatorSchema
+.virtual('url')
+.get(function () {
+  return '/' + this._id;
+});
+
+// Export the Schema as a mongoose model. A model instance can be considered an actual document to be saved/updated/deleted from a MongoDB collection
+module.exports = mongoose.model('Creator', CreatorSchema);
