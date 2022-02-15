1add96cc07bc58f4097cf5d4ba96c1fd9d66e3a8
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-15 14:24:17 +1030

    Add NFT collection controller with dummy route handlers/controller functions

---
+++ controllers/nftCollectionController.js
@@ -0,0 +1,41 @@
+const NftCollection = require('../models/nftCollection');
+
+// Display list of all NFT collections.
+exports.nftCollectionList = function (req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection list');
+};
+
+// Display details page for specific NFT collection.
+exports.nftCollectionDetail = function (req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection detail: ' + req.params.id);
+};
+
+// Display form for adding new NFT collection on GET.
+exports.addNftCollectionGet = function (req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection add GET');
+};
+
+// Handle adding new NFT collection on POST.
+exports.addNftCollectionPost = function (req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection add POST');
+};
+
+// Display NFT collection delete form on GET.
+exports.deleteNftCollectionGet = function(req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection delete GET');
+};
+
+// Handle NFT collection delete on POST.
+exports.deleteNftCollectionPost = function(req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection delete POST');
+};
+
+// Display NFT collection update form on GET.
+exports.updateNftCollectionGet = function(req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection update GET');
+};
+
+// Handle NFT collection update on POST.
+exports.updateNftCollectionPost = function(req, res) {
+  res.send('NOT IMPLEMENTED: NFT collection update POST');
+};
