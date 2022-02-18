920d87105881c4857768b63b173229ca58c7ddce
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-18 20:52:42 +1030

    Fix nft form bug and add basic error styling

--- controllers/nftController.js
+++ controllers/nftController.js
@@ -101,7 +101,7 @@ exports.addNftPost = [
 
   // Process request after input data has been validated and sanitised
   (req, res, next) => {
-
+    
     // Extract the validation errors from a request
     const errors = validationResult(req);
 
