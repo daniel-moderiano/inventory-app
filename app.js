0256783c8a49fbb7963ed15de0aa4a3301c3fa05
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-18 20:55:25 +1030

    Adjust for heroku deployment

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var mongoose = require('mongoose');
 var app = express();
 
 //Set up mongoose connection
-var mongoDB = 'mongodb+srv://daniel:daniel12@cluster0.54lv3.mongodb.net/inventory-app?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://daniel:daniel12@cluster0.54lv3.mongodb.net/inventory-app?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
