5c92ed2516b6ed9d5ef1b21a8085c715631f7363
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-02-19 19:52:00 +1030

    Add dotenv support

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -9,9 +10,7 @@ var mongoose = require('mongoose');
 var app = express();
 
 //Set up mongoose connection
-var dev_db_url = 'mongodb+srv://daniel:daniel12@cluster0.54lv3.mongodb.net/inventory-app?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
-
+var mongoDB = process.env.MONGODB_URI
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
